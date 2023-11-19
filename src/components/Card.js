6bed42fc4e254cf0ed4e086f8a64f62978401bc0
Pesala Sri Datta <datta.pesala@gmail.com>
2023-11-19 17:48:54 +0530

    Build - 1.0

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,21 @@
 import React from 'react'
-import pool from '../images/pool.svg'
 import star from '../images/star.svg'
 
-export default function Card(){
+export default function Card({img, rating, numRatings, name, price}){
 
     return(
         <div className='card'>
             <div className = 'pool'>
-            <img src = {pool}/>
+            <img src = {img}/>
             </div>
             <div className = 'star'>
             <img src ={star}/>
-            <p className='rating'>5.0</p>
-            <p className='ratinginfo'> (6)·USA</p>
+            <p className='rating'>{rating}</p>
+            <p className='ratinginfo'> ({numRatings})·USA</p>
             </div>
-            <p>Life lessons with Katie Zaferes</p>
+            <p className = 'sellerInfo'>{name}</p>
             <div className='price'>
-            <p className='pricep'>From $136 </p><p> / person</p>
+            <p className='pricep'>From ${price} </p><p> / person</p>
             </div>
         </div>
     )
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import Card from './Card'
+import pool from '../images/pool.svg'
+import wedding from '../images/wedding.svg'
+import mountain from '../images/mountain.svg'
 export default function Section(){
     return(
         <div className='section'>
-            <Card/>
-            <Card/>
-            <Card/>
+            <Card 
+            img = {pool}
+            name = "Life lessons with Katie Zaferes"
+            price = "139"
+            country = "USA"
+            numRatings = "6"
+            rating = "5"/>
+            <Card 
+            img = {wedding}
+            name = "Learn wedding photography"
+            price = "125"
+            country = "USA"
+            numRatings = "30"
+            rating = "5"/>            
+            <Card 
+            img = {mountain}
+            name = "Group Mountain Biking"
+            price = "50"
+            country = "USA"
+            numRatings = "2"
+            rating = "4.8"/>
         </div>
     )
 }
